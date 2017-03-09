f5b58cc7d89c7465700049fe52499e2001baf306
Jeremy Liss <jeremyliss@gmail.com>
2017-03-09 17:09:13 +1300

    add sounds and colours

--- drum.js
+++ drum.js
@@ -1,9 +1,24 @@
-document.getElementById('one').addEventListener('click', function() {
-  var audio = document.getElementById('kick')
-  audio.play()
-})
+// document.getElementById('one').addEventListener('click', function() {
+//   var audio = document.getElementById('kick')
+//   audio.play()
+// })
+
+kick()
+
+
+function kick () {
+
+  kick = document.getElementById('one')
+
+  kick.addEventListener('click', function() {
+    var audio = document.getElementById('kick')
+    audio.play()
+  })
+
+}
 
 document.getElementById('two').addEventListener('click', function() {
   var audio = document.getElementById('clp')
   audio.play()
+  .src = './images/red.jpg'
 })
