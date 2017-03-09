28e837be5ec911aba6d79000066f2c5c64d25225
Jeremy Liss <jeremyliss@gmail.com>
2017-03-09 16:04:36 +1300

    setup

---
+++ drum.js
@@ -0,0 +1,9 @@
+document.getElementById('one').addEventListener('click', function() {
+  var audio = document.getElementById('kick')
+  audio.play()
+})
+
+document.getElementById('two').addEventListener('click', function() {
+  var audio = document.getElementById('clp')
+  audio.play()
+})
