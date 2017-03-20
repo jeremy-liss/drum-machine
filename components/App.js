9c0d62379991c0a165f721ecfb6a867eed0ef76f
Jeremy Liss <jeremyliss@gmail.com>
2017-03-20 20:37:52 +1300

    add rows

--- components/App.js
+++ components/App.js
@@ -3,14 +3,37 @@ var React = require('react')
 module.exports = function (props){
 
   return (
-    <div id="pads">
-      {props.pads.map(function(pad) {
-        return (
-          <div id="pad">
-            {pad.isRunning ? <img src={props.images.red} /> : <img src={props.images.black} />}
-          </div>
-        )
-      })}
-    </div>
+    <div>
+      <div id="pads">
+        Kick
+        {props.pads.map(function(pad) {
+          return (
+            <div id="pad">
+              {pad.isRunning ? <img src={props.images.red} /> : <img src={props.images.black} />}
+            </div>
+          )
+        })}
+      </div>
+      <div id="pads">
+        Snare
+        {props.pads.map(function(pad) {
+          return (
+            <div id="pad">
+              {pad.isRunning ? <img src={props.images.red} /> : <img src={props.images.black} />}
+            </div>
+          )
+        })}
+      </div>
+      <div id="pads">
+        Hi-Hat
+        {props.pads.map(function(pad) {
+          return (
+            <div id="pad">
+              {pad.isRunning ? <img src={props.images.red} /> : <img src={props.images.black} />}
+            </div>
+          )
+        })}
+      </div>
+  </div>
   )
 }
--- data/initialState.js
+++ data/initialState.js
@@ -1,17 +1,19 @@
 module.exports = {
   pads:[
-    {id: 0, isRunning: false, image: '../images/red.jpg' },
-    { id: 1, isRunning: false, image: '../images/black.jpg', },
-    { id: 2, isRunning: false, image: '../images/black.jpg', },
-    { id: 3, isRunning: false, image: '../images/black.jpg', }
+    { id: 0, isRunning: false },
+    { id: 1, isRunning: false },
+    { id: 2, isRunning: false },
+    { id: 3, isRunning: false }
   ],
 
-  images: {black: '../images/black.jpg',
-          red: '../images/red.jpg'
+  images: {
+    black: '../images/black.jpg',
+    red: '../images/red.jpg'
           },
 
-  sounds: {kick: '../sounds/kick.wav',
-           snare: '../sounds/clp.wav'        
+  sounds: {
+    kick: '../sounds/kick.wav',
+    snare: '../sounds/clp.wav'
           }
 
 }
